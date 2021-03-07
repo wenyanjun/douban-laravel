eebe9af34e1651bf7e9e52cd2679cc1deb5f42aa
Ëæ·ç <997745354@qq.com>
2021-03-07 13:43:27 +0800

    修改返回头部信息

--- app/helpers.php
+++ app/helpers.php
@@ -2,6 +2,10 @@
 function common(){
     $obj = [];
     $obj['coderYJ'] = '简书关注coderYJ 欢迎加QQ群讨论277030213';
+    $obj["money"] = "给我打赏 https://blog.csdn.net/simplyou/article/details/113424779?spm=1001.2014.3001.5501";
+    $obj["course"] = "微信小程序课程 https://www.bilibili.com/video/BV1ia4y1H794";
+    $obj["www.itjava.cn"] = "API文档 www.itjava.cn";
+    $obj["idea"] = "idea 激活 https://www.jianshu.com/p/28f4e9a6c7c8";
     return $obj;
 }
 // 应用公共文件
